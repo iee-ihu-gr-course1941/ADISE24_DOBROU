8076f6c9136bef0838f951b536bd0f1bbe0c93e6
DeanDobrou <cdob18@gmail.com>
2025-01-10 22:26:23 +0200

    new db config

--- config/Database.php
+++ config/Database.php
@@ -2,7 +2,6 @@
 class Database
 {
     private static $host = 'localhost';
-    private static $port = '3306';
     private static $db_name = 'ataxx';
     private static $username = 'it185404';
     private static $password = '';
@@ -10,13 +9,31 @@ class Database
 
     public static function connect()
     {
-        if (self::$conn == null) {
-            self::$conn = new mysqli(self::$host, self::$username, self::$password, self::$db_name, self::$port);
+        if (self::$conn === null) {
+            if (gethostname() == 'users.iee.ihu.gr') {
+                // University server configuration with socket
+                self::$conn = new mysqli(
+                    self::$host,
+                    self::$username,
+                    self::$password,
+                    self::$db_name,
+                    null,
+                    '/home/student/it/2018/it185404/mysql/run/mysql.sock'
+                );
+            } else {
+                self::$conn = new mysqli(
+                    self::$host,
+                    self::$username,
+                    self::$password,
+                    self::$db_name
+                );
+            }
 
             if (self::$conn->connect_error) {
-                die('Database Connection Failed: ' . self::$conn->connect_error);
+                die("Database Connection Failed: " . self::$conn->connect_error);
             }
         }
+
         return self::$conn;
     }
 }
